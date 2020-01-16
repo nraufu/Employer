94a7de3a93db8249724e721197b721253b433fd6
nraufu <nraufu@gmail.com>
2020-01-16 11:16:30 +0200

    ft(test): add more tests - add manager enpoint tests

--- server/test/manager.test.js
+++ server/test/manager.test.js
@@ -1,7 +1,7 @@
 import chai from 'chai';
 import chaiHttp from 'chai-http';
 import app from '../index';
-
+import sample from './sampleData';
 
 const {
     expect
@@ -20,3 +20,35 @@ describe('/GET API', () => {
     });
 });
 
+describe('/POST signUp', () => {
+    it('should return 201 status created code when new manager signup successfully', (done) => {
+        chai
+            .request(app)
+            .post('/auth/signup')
+            .send(sample.validManager)
+            .end((err, res) => {
+                expect(res).to.have.status(201);
+                done();
+            });
+    });
+    it('should return 409 conflict status code when manager with same email, phoneNumber or id already exist', (done) => {
+        chai
+        .request(app)
+        .post('/auth/signup')
+        .send(sample.validManager)
+        .end((err, res) => {
+            expect(res).to.have.status(409);
+            done();
+        });
+    });
+    it('should return 400 bad request status code when submit invalid values', (done) => {
+        chai
+        .request(app)
+        .post('/auth/signup')
+        .send(sample.invalidManager)
+        .end((err, res) => {
+            expect(res).to.have.status(400);
+            done();
+        });
+    });
+});
